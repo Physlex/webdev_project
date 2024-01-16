93d7c85ffc10a124b6427f9f6a9f1008fa46f883
Physlex <al.willet02@gmail.com>
2024-01-15 18:39:06 -0700

    finished hooking up unit tests

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys, sqlite3
+from logging import Logger
+
+def plus_one(x):
+    return x + 1
+
+if __name__ == "__main__":
+    print(sys.path)
+    sys.exit(0)
+    pass
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from src import main
+
+def test_main():
+    assert (main.plus_one(2) == 3)
+
+if __name__  == "__main__":
+    pass
